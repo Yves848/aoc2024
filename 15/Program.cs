d3691d446c4257463a7835e2f380971cf794ac8f
yves848 <yves.godart@gmail.com>
2024-12-15 14:36:31 +0100

    15-2 WIP

--- 15/Program.cs
+++ 15/Program.cs
@@ -5,7 +5,7 @@ if (args.Length > 0)
 {
   input = File.ReadAllText(args[0]);
 }
-else input = File.ReadAllText(@"C:\Users\yvesg\git\aoc2024\15\test2.txt");
+else input = File.ReadAllText(@"C:\Users\yvesg\git\aoc2024\15\test3.txt");
 var parts = input.Split("\r\n\r\n");
 
 int gl = parts[0].Length;
@@ -29,6 +29,48 @@ void parseGrid()
     if (l.Contains('@'))
     {
       robot.Item1 = l.IndexOf('@');
+      robot.Item2 = r;
+    }
+    r++;
+  });
+}
+
+void parseGrid2()
+{
+  int r = 0;
+  parts[0].Split("\r\n").ToList().ForEach(l =>
+  {
+    string line = "";
+    l.ToList().ForEach(c =>
+    {
+      string ch = c.ToString();
+      if (ch == "#" || ch == "O")
+      {
+        if (ch == "#")
+        {
+          line += "".PadLeft(2, '#');
+        }
+        else
+        {
+          line += "[]";
+        }
+      }
+      else
+      {
+        if (ch == "@")
+        {
+          line += "@.";
+        }
+        else
+        {
+          line += "..";
+        }
+      }
+    });
+    grid.Add(line);
+    if (line.Contains('@'))
+    {
+      robot.Item1 = line.IndexOf('@');
       robot.Item2 = r;
     }
     r++;
@@ -124,6 +166,106 @@ void moveRobot(char direction)
   }
 }
 
+void moveRobot2(char direction)
+{
+  if (directions.ContainsKey(direction))
+  {
+    // direction to move
+    var (dc, dr) = directions[direction];
+    // robot position
+    var (rc, rr) = robot;
+    while (true)
+    {
+      // check next spot
+      if (direction == '<' || direction == '>')
+      {
+        int nc = rc + dc;
+        int nr = rr + dr;
+        if (nr > grid.Count - 1 || nr < 0 || nc > grid[0].Length - 1 || nc < 0) break;
+        string target = grid[nr].Substring(nc, 1);
+        if (target == "#") break;
+        if (target == ".")
+        {
+          // free spot, move robot
+          grid[rr] = grid[rr].Remove(rc, 1).Insert(rc, ".");
+          grid[nr] = grid[nr].Remove(nc, 1).Insert(nc, "@");
+          robot.Item1 = nc;
+          robot.Item2 = nr;
+          break;
+        }
+        if ("[]".Contains(target))
+        {
+          // deterine if there is room to move while queuing the crates.
+          HashSet<(int, int, string)> crates = [(nr, nc, target)];
+          bool room = false; // first free spot to check
+          int fr = nr, fc = nc;
+          while (true)
+          {
+            fr += dr;
+            fc += dc;
+            if (fr > grid.Count - 1 || fr < 0 || fc > grid[0].Length - 1 || fc < 0) break;
+            string free = grid[fr].Substring(fc, 1);
+            if ("[]".Contains(free))
+            {
+              crates.Add((fr, fc, free));
+            }
+            if (free == ".")
+            {
+              room = true;
+              break;
+            }
+            if (free == "#")
+            {
+              break;
+            }
+          }
+          if (room)
+          {
+            crates.ToList().ForEach(crate =>
+            {
+              var (r, c, _) = crate;
+              grid[r] = grid[r].Remove(c, 1).Insert(c, ".");
+            });
+            crates.ToList().ForEach(crate =>
+            {
+              var (r, c, ch) = crate;
+              grid[r + dr] = grid[r + dr].Remove(c + dc, 1).Insert(c + dc, ch);
+            });
+            grid[rr] = grid[rr].Remove(rc, 1).Insert(rc, ".");
+            grid[nr] = grid[nr].Remove(nc, 1).Insert(nc, "@");
+            robot.Item1 = nc;
+            robot.Item2 = nr;
+          }
+
+          break;
+        }
+        break;        // horizontal
+      }
+      else
+      {
+        int nc = rc + dc;
+        int nr = rr + dr;
+        if (nr > grid.Count - 1 || nr < 0 || nc > grid[0].Length - 1 || nc < 0) break;
+        string target = grid[nr].Substring(nc, 1);
+        if (target == "#") break;
+        if (target == ".")
+        {
+          // free spot, move robot
+          grid[rr] = grid[rr].Remove(rc, 1).Insert(rc, ".");
+          grid[nr] = grid[nr].Remove(nc, 1).Insert(nc, "@");
+          robot.Item1 = nc;
+          robot.Item2 = nr;
+          break;
+        }
+        break;
+      }
+    }
+    drawGrid();
+    Console.WriteLine($"direction {direction}");
+    Console.ReadLine();
+  }
+}
+
 int getGPS()
 {
   int result = 0;
@@ -134,7 +276,7 @@ int getGPS()
     {
       col = grid[row].IndexOf("O", col + 1);
       if (col == -1) break;
-      result += row*100 + col;
+      result += row * 100 + col;
     }
   }
   return result;
@@ -151,9 +293,11 @@ void part1()
 
 void part2()
 {
-
+  parseGrid2();
+  drawGrid();
+  parts[1].Trim().ToList().ForEach(moveRobot2);
 }
 
-part1();
+// part1();
 part2();
 
